4742599373e16612d7d04d6c1661f08ef62b7aa0
yepeichu123 <peichu.ye@gmail.com>
2020-03-02 19:26:47 +0800

    add nothing

---
+++ pcl_practice/ReadPointsFromImg/build/CMakeFiles/ReadPointsFromImg.dir/DependInfo.cmake
@@ -0,0 +1,64 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/xiaoc/xiaoc/code/tool_practice/pcl_practice/ReadPointsFromImg/src/ReadPointsFromImg.cpp" "/home/xiaoc/xiaoc/code/tool_practice/pcl_practice/ReadPointsFromImg/build/CMakeFiles/ReadPointsFromImg.dir/src/ReadPointsFromImg.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "DISABLE_DAVIDSDK"
+  "DISABLE_DSSDK"
+  "DISABLE_ENSENSO"
+  "DISABLE_LIBUSB_1_0"
+  "DISABLE_PCAP"
+  "DISABLE_PNG"
+  "DISABLE_RSSDK"
+  "EIGEN_USE_NEW_STDVECTOR"
+  "EIGEN_YES_I_KNOW_SPARSE_MODULE_IS_NOT_STABLE_YET"
+  "FLANN_STATIC"
+  "qh_QHpointer"
+  "vtkFiltersFlowPaths_AUTOINIT=1(vtkFiltersParallelFlowPaths)"
+  "vtkIOExodus_AUTOINIT=1(vtkIOParallelExodus)"
+  "vtkIOGeometry_AUTOINIT=1(vtkIOMPIParallel)"
+  "vtkIOImage_AUTOINIT=1(vtkIOMPIImage)"
+  "vtkIOSQL_AUTOINIT=2(vtkIOMySQL,vtkIOPostgreSQL)"
+  "vtkRenderingContext2D_AUTOINIT=1(vtkRenderingContextOpenGL)"
+  "vtkRenderingCore_AUTOINIT=4(vtkInteractionStyle,vtkRenderingFreeType,vtkRenderingFreeTypeOpenGL,vtkRenderingOpenGL)"
+  "vtkRenderingFreeType_AUTOINIT=2(vtkRenderingFreeTypeFontConfig,vtkRenderingMatplotlib)"
+  "vtkRenderingLIC_AUTOINIT=1(vtkRenderingParallelLIC)"
+  "vtkRenderingVolume_AUTOINIT=1(vtkRenderingVolumeOpenGL)"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/opt/ros/kinetic/include/opencv-3.3.1-dev"
+  "/opt/ros/kinetic/include/opencv-3.3.1-dev/opencv"
+  "/usr/include/vtk-6.2"
+  "/usr/include/tcl"
+  "/usr/include/freetype2"
+  "/usr/include/x86_64-linux-gnu/freetype2"
+  "/usr/include/jsoncpp"
+  "/usr/include/libxml2"
+  "/usr/include/x86_64-linux-gnu"
+  "/usr/include/python2.7"
+  "/usr/include/hdf5/openmpi"
+  "/usr/lib/openmpi/include/openmpi/opal/mca/event/libevent2021/libevent"
+  "/usr/lib/openmpi/include/openmpi/opal/mca/event/libevent2021/libevent/include"
+  "/usr/lib/openmpi/include"
+  "/usr/lib/openmpi/include/openmpi"
+  "/usr/local/include/pcl-1.8"
+  "/usr/include/eigen3"
+  "/usr/include/ni"
+  "/usr/include/openni2"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ pcl_practice/ReadPointsFromImg/build/CMakeFiles/ReadPointsFromImg.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ReadPointsFromImg.dir/src/ReadPointsFromImg.cpp.o"
+  "../bin/ReadPointsFromImg.pdb"
+  "../bin/ReadPointsFromImg"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ReadPointsFromImg.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
